3e0dbc9d09043f3fb3004290f917563f5434ebdf
Farmerc92 <farmerc@udel.edu>
2020-04-06 18:40:57 -0400

    fixed

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -50,7 +50,7 @@ public abstract class People<E extends Person>{
 
     abstract public E[] toArray();
 
-    public Iterator iterator(){
+    public Iterator<E> iterator(){
         return this.personList.iterator();
     }
 }
